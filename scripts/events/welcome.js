e3bcccd926033956e8b6fb8b6cd0298a5e03f625
Heyxd12 <btsarmh3215@gmail.com>
2024-10-13 15:46:20 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -3,7 +3,7 @@ module.exports = {
         if (msg.new_chat_members) {
             const newMembers = msg.new_chat_members.map(member => member.first_name).join(', ');
             const chatName = msg.chat.title || 'this group';
-            const welcomeMessage = `Welcome ${newMembers} to ${chatName}!`;
+            const welcomeMessage = `Welcome Hello Iam Gojo Chat Bot My Owner MaHi Ahmed🌸I hope You enjoy ${newMembers} to ${chatName}!`;
 
             bot.sendMessage(msg.chat.id, welcomeMessage);
         }
